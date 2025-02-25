9d6176f7539ce75a624590fe3f229c1a7ca5228b
anonymous <nitinswami12577@gmail.com>
2025-02-25 19:23:24 +0530

    fix redirect

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,6 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
   // Redirect to the signup page
-  redirect('/signup');
+  // redirect('/signup');
   return null; 
 }
