d0d0494c9e6c60eb48f2949ec059f516303ff990
Anonymous <116019683+Annonymous-001@users.noreply.github.com>
2025-02-25 19:28:47 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,6 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
   // Redirect to the signup page
-  // redirect('/signup');
+  // redirect('/blogs');
   return null; 
 }
