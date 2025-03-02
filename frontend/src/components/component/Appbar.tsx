5916d195ba3ba3fb2214039b59af4cca39134b7c
anonymous <nitinswami12577@gmail.com>
2025-03-02 18:30:48 +0530

    rollback

--- frontend/src/components/component/Appbar.tsx
+++ frontend/src/components/component/Appbar.tsx
@@ -32,14 +32,14 @@ export default function Appbar() {
   return (
     <nav className="flex items-center justify-between bg-background border-b-2 mb-6 pb-3 pt-0">
       <div className="flex items-center space-x-4">
-        {/* <span className="text-xl font-bold">Verse Trail</span> */}
-        <Image
+        <span className="text-xl font-bold">Verse Trail</span>
+        {/* <Image
         src="/logo.png"
         alt="Verse Trail Logo"
         width={40}
         height={40}
         priority
-        />
+        /> */}
       </div>
 
       <div className="flex items-center space-x-4">
